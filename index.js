17534315545240d59b2a7a4566d916d1ecfec8ae
Ubuntu <ubuntu@ip-172-31-45-122.us-east-2.compute.internal>
2018-02-06 20:53:37 +0000

    Made a few changes to run on heroku server

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
 	console.log('a user is connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
 	socket.on('chat message', function(msg){
-		io.emit('some event', {for : 'everyone'});
-		io.emit('chat message', msg);
-		console.log('message:\t' + msg);
-	});
-	socket.broadcast.emit('hi');
+	    io.emit('chat message', msg);
+	  });
 });
 
 
-http.listen(443, function(){
-	console.log('listen on *:80');
+http.listen(port, function(){
+	console.log('listen on *:80' + port);
 });
